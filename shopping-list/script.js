583e498de3352ea63cf8a3d26cbdf471e175877c
Roshan Kumar <Roshan.KumarC@aionos.ai>
2025-07-25 21:25:26 +0530

    shopping list project start

---
+++ shopping-list/script.js
@@ -0,0 +1,40 @@
+const formItem = document.getElementById("item-form");
+const formInput = document.getElementById("item-input");
+const itemList = document.getElementById("item-list");
+
+function addItem(e) {
+  e.preventDefault();
+
+  const newItem = formInput.value.trim();
+  if (newItem === "") {
+    alert("Please enter an item.");
+  }
+
+  const listItem = document.createElement("li");
+  listItem.appendChild(document.createTextNode(newItem));
+
+  const deleteButton = createButton("remove-item btn-link text-red");
+
+  listItem.appendChild(deleteButton);
+  itemList.appendChild(listItem);
+  formInput.value = "";
+}
+
+function createButton(classes) {
+  const button = document.createElement("button");
+  button.className = classes;
+
+  const icon = createIcon("fa-solid fa-xmark");
+
+  button.appendChild(icon);
+
+  return button;
+}
+
+function createIcon(classes) {
+  const icon = document.createElement("i");
+  icon.className = classes;
+  return icon;
+}
+
+formItem.addEventListener("submit", addItem);
